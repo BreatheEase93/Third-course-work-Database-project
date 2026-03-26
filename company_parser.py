de09885a73e6dfdbbbb7ed4951bfc3018d9b0777
Дмитрий Глоба <dima.globa2015@yandex.ru>
2026-03-26 18:14:41 +0800

    Начало

---
+++ company_parser.py
@@ -0,0 +1,21 @@
+COMPANIES = [
+    {'id': 1, 'name': 'Яндекс', 'employer_id': 1740},
+    {'id': 2, 'name': 'СБЕР', 'employer_id': 3529},
+    {'id': 3, 'name': 'X5 Tech', 'employer_id': 9352463},
+    {'id': 4, 'name': 'Лаборатория Касперского', 'employer_id': 1057},
+    {'id': 5, 'name': 'OZON', 'employer_id': 2180},
+    {'id': 6, 'name': 'МТС', 'employer_id': 3776},
+    {'id': 7, 'name': 'IBS', 'employer_id': 139},
+    {'id': 8, 'name': 'Росатом', 'employer_id': 107434},
+    {'id': 9, 'name': 'КРОК', 'employer_id': 2987},
+    {'id': 10, 'name': 'ООО ИЦ АЙ-ТЕКО', 'employer_id': 872178}
+]
+
+url = "https://api.hh.ru/vacancies"
+
+professional_role = [
+        10, 73, 83, 96, 104, 107, 112, 113, 114, 124, 125,
+        148, 150, 156, 157, 158,  160, 164, 165
+    ]
+
+
